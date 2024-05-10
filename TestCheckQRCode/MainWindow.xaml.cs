a6dcbfd4d8b0b7d4ab8eaddab47e1fe91206f37a
Windows <vtnduc2382000@gmail.com>
2024-05-10 11:44:46 +0700

    Add project files.

---
+++ TestCheckQRCode/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System.ComponentModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TestCheckQRCode
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //private string captcha;
+
+        //public string Captcha
+        //{
+        //    get { return captcha; }
+        //    set
+        //    {
+        //        captcha = value;
+        //        OnPropertyChanged(nameof(Captcha));
+        //    }
+        //}
+        public MainWindow()
+        {
+            InitializeComponent();
+            //this.DataContext = this;
+            //GenerateCaptcha();
+        }
+        //private void GenerateCaptcha()
+        //{
+        //    Random random = new Random();
+        //    const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        //    char[] captchaChars = new char[6];
+        //    for (int i = 0; i < 6; i++)
+        //    {
+        //        captchaChars[i] = chars[random.Next(chars.Length)];
+        //    }
+        //    Captcha = new string(captchaChars);
+        //}
+
+        //private void CheckCaptcha_Click(object sender, RoutedEventArgs e)
+        //{
+        //    if (captchaTextBox.Text == Captcha)
+        //    {
+        //        resultTextBlock.Text = "CAPTCHA đúng!";
+        //        GenerateCaptcha();
+        //    }
+        //    else
+        //    {
+        //        resultTextBlock.Text = "CAPTCHA sai. Vui lòng thử lại.";
+        //        GenerateCaptcha();
+        //    }
+        //    captchaTextBox.Text = "";
+        //}
+
+        //public event PropertyChangedEventHandler PropertyChanged;
+
+        //protected virtual void OnPropertyChanged(string propertyName)
+        //{
+        //    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        //}
+    }
+}
---
+++ TestCheckQRCode/ViewModel/MainViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace TestCheckQRCode.ViewModel
+{
+    public class MainViewModel:BaseViewModel
+    {
+        private string _Captcha;
+        public string Captcha { get=>_Captcha; set { _Captcha = value;OnPropertyChanged(); } }
+        private string _ResultCheckCaptcha;
+        public string ResultCheckCaptcha { get=>_ResultCheckCaptcha; set { _ResultCheckCaptcha = value;OnPropertyChanged(); } }
+        private string _UserCaptcha;
+        public string UserCaptcha { get=>_UserCaptcha; set { _UserCaptcha = value;OnPropertyChanged(); } }
+        public MainViewModel()
+        {
+            GenerateCaptcha();
+            CheckCaptchaCommand = new RelayCommand<object>((p) => true, (p) => CheckCapt(p));
+        }
+        
+        private void CheckCapt(object p)
+        {
+            if (UserCaptcha == Captcha)
+            {
+                ResultCheckCaptcha = "CAPTCHA đúng!";
+                GenerateCaptcha();
+            }
+            else
+            {
+                ResultCheckCaptcha = "CAPTCHA sai. Vui lòng thử lại.";
+                GenerateCaptcha();
+            }
+            UserCaptcha= "";
+        }
+        private void GenerateCaptcha()
+        {
+            Random random = new Random();
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-=.?/[],;'|_+@!#$%^&*";
+            char[] captchaChars = new char[6];
+            for (int i = 0; i < 6; i++)
+            {
+                captchaChars[i] = chars[random.Next(chars.Length)];
+            }
+            Captcha = new string(captchaChars);
+        }
+        public ICommand CheckCaptchaCommand { get; set; }
+    }
+}
